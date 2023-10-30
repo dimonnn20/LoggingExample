f7c1eab53e6564225f72440ed58eeae975f8fef7
dimonnn20 <dimonnn20@gmail.com>
2023-10-30 21:35:27 +0100

    Добавьте файлы проекта.

---
+++ LoggingExample/Logger.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingExample
+{
+    public static class Logger
+    {
+        public static ILog log { get; } = LogManager.GetLogger(typeof(Logger));
+        static Logger()
+        { 
+        XmlConfigurator.Configure(new System.IO.FileInfo("C:/Users/makad/source/repos/LoggingExample/LoggingExample/log4net.config"));
+        }
+
+    }
+}
